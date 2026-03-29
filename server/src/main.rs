e98988fefa5958d591d5656933bcf0dc476804b5
Kenny Gatdula <kennyg@pobox.com>
2026-03-29 06:46:35 -0400

    Clean up debug logging from LSP server

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,6 +5,7 @@ use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+
 #[derive(Debug)]
 struct GitNotesLsp {
     client: Client,
